61227dda460074ea220855411d19d7aeffabd9f4
Albert <albert@alberts-mac.local>
2017-11-14 11:20:53 +0100

    Initial Commit

---
+++ Shutterbug/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Shutterbug
+//
+//  Created by Albert on 14/11/17.
+//  Copyright © 2017 Albert. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
